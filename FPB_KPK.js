a85f9ba45d098ff047c3f2cb924db41d3f75be96
waayy <44268485+waayy@users.noreply.github.com>
2018-11-06 13:20:44 +0700

    Update FPB_KPK.js

--- FPB_KPK.js
+++ FPB_KPK.js
@@ -1,20 +1,20 @@
 
-function fpb_kpk(x,y){
-    var fpb=0;
+// function fpb_kpk(x,y){
+//     var fpb=0;
     
-    for(var a=1; a<=x; a++){
-        var b = x%a
-        var c = y%a
-        if(b==c && c==0){
-            var fpb = a
-        }
-    }
+//     for(var a=1; a<=x; a++){
+//         var b = x%a
+//         var c = y%a
+//         if(b==c && c==0){
+//             var fpb = a
+//         }
+//     }
 
-    var kpk = (x*y)/fpb
+//     var kpk = (x*y)/fpb
     
-    console.log('fpb' + x + ' & ' + y + ' = ' + fpb)
-    console.log('kpk' + x + ' & ' + y + ' = ' + kpk)
-}
+//     console.log('fpb' + x + ' & ' + y + ' = ' + fpb)
+//     console.log('kpk' + x + ' & ' + y + ' = ' + kpk)
+// }
 
-fpb_kpk (2,3)
-fpb_kpk (3,2)
+// fpb_kpk (2,3)
+// fpb_kpk (3,2)
