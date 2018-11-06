a45f98640a5dfdd0ef665a24b17fcddb4447737e
waayy <44268485+waayy@users.noreply.github.com>
2018-11-06 13:18:41 +0700

    fpb_kpk

---
+++ FPB_KPK.js
@@ -0,0 +1,20 @@
+
+function fpb_kpk(x,y){
+    var fpb=0;
+    
+    for(var a=1; a<=x; a++){
+        var b = x%a
+        var c = y%a
+        if(b==c && c==0){
+            var fpb = a
+        }
+    }
+
+    var kpk = (x*y)/fpb
+    
+    console.log('fpb' + x + ' & ' + y + ' = ' + fpb)
+    console.log('kpk' + x + ' & ' + y + ' = ' + kpk)
+}
+
+fpb_kpk (2,3)
+fpb_kpk (3,2)
